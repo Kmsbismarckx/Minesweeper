a66f2b3f02d2ebcd6f046d5581a5b32e77479f00
Bismarck <klimentmaslennikov@gmail.com>
2022-07-06 13:58:30 +0300

    add quantity of mines in cells

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ function gameStart(size, bombsCount) {
   const field = document.querySelector('.field');
   const cellsCount = Math.pow(size, 2);
 
-
   field.style.gridTemplateColumns = `repeat(${size}, 40px)`;
   field.innerHTML = '<button></button>'.repeat(cellsCount);
   const cells = [...field.children]
@@ -10,7 +9,6 @@ function gameStart(size, bombsCount) {
   const bombs = [...Array(cellsCount).keys()]
     .sort(() => Math.random() - 0.5)
     .slice(0, bombsCount);
-  console.log(bombs);
 
   field.addEventListener('click', (event) => {
     if (event.target.tagName !== 'BUTTON') {
@@ -26,16 +24,80 @@ function gameStart(size, bombsCount) {
 function open(row, column) {
   const index = row * size + column;
   const cell = cells[index];
-  cell.innerHTML = isBomb(row, column) ? 'X' : ' ';
+  cell.innerHTML = isBomb(row, column) ? 'X' : bombsCountHandler(row, column, index);
   cell.disabled = true;
+  count = 0;
 }
+  let count = 0;
 
   function isBomb(row, column) {
     const index = row * size + column;
+    if (bombs.includes(index)) {
+      count++;
+      console.log('Бомба тут - ' + index);
+    }
 
     return bombs.includes(index)
   }
+
+  function bombsCountHandler(row, column, index) {
+
+    if (index === 0) {
+      isBomb(row, column + 1);
+      isBomb(row + 1, column);
+      isBomb(row + 1, column + 1);
+    } else if (index === size - 1) {
+      isBomb(row, column - 1);
+      isBomb(row + 1, column);
+      isBomb(row + 1, column - 1);
+    } else if (index === size * size - size) {
+      isBomb(row, column + 1);
+      isBomb(row - 1, column);
+      isBomb(row - 1, column + 1);
+    } else if (index === size * size - 1) {
+      isBomb(row, column - 1);
+      isBomb(row - 1, column);
+      isBomb(row - 1, column - 1);
+    } else if (index <= size * size - 1 && index >= size * size - size) {
+      isBomb(row, column + 1);
+      isBomb(row, column - 1);
+      isBomb(row - 1, column);
+      isBomb(row - 1, column + 1);
+      isBomb(row - 1, column - 1);
+    } else if (index >= 0 && index <= size - 1) {
+      isBomb(row, column + 1);
+      isBomb(row, column - 1);
+      isBomb(row + 1, column);
+      isBomb(row + 1, column + 1);
+      isBomb(row + 1, column - 1);
+    } else if (index % size === 0 || index === 0) {
+      isBomb(row, column + 1);
+      isBomb(row + 1, column);
+      isBomb(row - 1, column);
+      isBomb(row + 1, column + 1);
+      isBomb(row - 1, column + 1);
+    } else if (index === size - 1 || index % size - 1 === 0  ) {
+      isBomb(row, column - 1);
+      isBomb(row + 1, column);
+      isBomb(row - 1, column);
+      isBomb(row - 1, column - 1);
+      isBomb(row + 1, column - 1);
+    } else {
+      isBomb(row, column + 1);
+      isBomb(row, column - 1);
+      isBomb(row - 1, column);
+      isBomb(row + 1, column);
+      isBomb(row - 1, column - 1);
+      isBomb(row + 1, column - 1);
+      isBomb(row + 1, column + 1);
+      isBomb(row - 1, column + 1);
+    }
+    return count;
+  }
+
 }
+
+
 
 function init(size, bombsCount) {
   gameStart(size, bombsCount);
